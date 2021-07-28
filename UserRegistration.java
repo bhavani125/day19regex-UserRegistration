317d501564ffc6e42d02a88d89d6d182b8fe1b5b
bhavani125 <girinenibhavani12@gmil.com>
2021-07-28 21:37:18 +0530

    Added: Code of validating phoneNumber

--- UserRegistration.java
+++ UserRegistration.java
@@ -7,6 +7,7 @@ public class UserRegistration {
     Scanner sc = new Scanner(System.in);
     public static final String namePattern = "[A-Z]{1}[A-Za-z]{2,}";
     public static final String emailPattern ="([0-9A-Za-z])+([-+._][0-9A-Za-z]+)*" + "@([0-9A-Za-z])+[.]([a-zA-Z])+([.][A-Za-z]+)*";
+    public static final String numberPattern="^[+0-9]{2}{10}$";
     //Main Method
     public static void main(String[] args) {
         System.out.println("Welcome to User Registration program");
@@ -27,13 +28,19 @@ public class UserRegistration {
 
         System.out.println("Enter last name ");
         String lastName = sc.next();
-        //calling validateLastName method
+        //calling validateInput method
         validateInput(namePattern,lastName);
 
         System.out.println("Enter email id ");
         String emailId = sc.next();
-        //calling validateEmailId method
+        //calling validateInputmethod
         validateInput(emailPattern,emailId);
+
+        System.out.println("Enter phoneNumber");
+        String phoneNumber = sc.next();
+        //calling validateInput method
+        validateInput(numberPattern,phoneNumber);
+
     }
     //Creating validateInput method  is to check
     private void validateInput(String pattern, String input) {
